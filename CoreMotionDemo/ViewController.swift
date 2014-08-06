34a0e99f2baa1505c5b9818772d278ac812d5e71
BJ Miller <bj@sixfivesoftware.com>
2014-08-06 14:53:46 -0400

    Add initial UI and hook up outlets.

--- CoreMotionDemo/ViewController.swift
+++ CoreMotionDemo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var stepLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var lastUpdatedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
