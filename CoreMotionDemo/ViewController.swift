b7234a4983799b598080721aa9eab3ec73999aeb
BJ Miller <bj@sixfivesoftware.com>
2014-08-06 14:46:19 -0400

    Initial commit.

---
+++ CoreMotionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMotionDemo
+//
+//  Created by BJ Miller on 8/6/14.
+//  Copyright (c) 2014 Six Five Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
